c3d2a4c49a19182af08c256112783abf00a577d6
Florian Mierzejewski <florianmski@novoda.com>
2018-06-14 18:43:28 +0200

    make sure to trim replacement character

--- slugger.go
+++ slugger.go
@@ -6,8 +6,9 @@ import (
 )
 
 // SlugifyTitle : Make sure to clean up the branch names
-func SlugifyTitle(ticketTitle string) string {
-	re := regexp.MustCompile("[^a-z0-9]+")
-
-	return strings.Trim(re.ReplaceAllString(strings.ToLower(ticketTitle), "-"), "-")
+func SlugifyTitle(ticketTitle string, replacementCharacter string) string {
+	re := regexp.MustCompile("[^a-z0-9]+");
+	lowerCaseTitle := strings.ToLower(ticketTitle)
+	formattedTitle := re.ReplaceAllString(lowerCaseTitle, replacementCharacter)
+	return strings.Trim(formattedTitle, replacementCharacter)
 }
--- slugger_test.go
+++ slugger_test.go
@@ -13,15 +13,20 @@ var _ = Suite(&SluggerSuite{})
 
 func (s *SluggerSuite) TestSlugCreationNormalText(c *C) {
 	title := "This is a fake title"
-	c.Assert(SlugifyTitle(title), Equals, "this-is-a-fake-title")
+	c.Assert(SlugifyTitle(title, "-"), Equals, "this-is-a-fake-title")
 }
 
 func (s *SluggerSuite) TestSlugCreationWithSpecialChars(c *C) {
 	secondTitle := "This &&& *** is another title"
-	c.Assert(SlugifyTitle(secondTitle), Equals, "this-is-another-title")
+	c.Assert(SlugifyTitle(secondTitle, "_"), Equals, "this_is_another_title")
 }
 
 func (s *SluggerSuite) TestSlugCreationWithMultipleSpaces(c *C) {
 	title := "This is a                    fake title"
-	c.Assert(SlugifyTitle(title), Equals, "this-is-a-fake-title")
+	c.Assert(SlugifyTitle(title, "+"), Equals, "this+is+a+fake+title")
+}
+
+func (s *SluggerSuite) TestSlugCreationWithSpecialCharAtTheEnd(c *C) {
+	title := "This is a fake (title)"
+	c.Assert(SlugifyTitle(title, "+"), Equals, "this+is+a+fake+title")
 }
